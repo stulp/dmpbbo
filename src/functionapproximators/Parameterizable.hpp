919fd7fe8ee72e80f121df10efaf155b3dacedc4
Freek Stulp <freek.stulp@ensta-paristech.fr>
2014-11-06 11:38:13 +0100

    Gennaro added REALTIME_CHECKS option.

--- src/functionapproximators/Parameterizable.hpp
+++ src/functionapproximators/Parameterizable.hpp
@@ -24,28 +24,31 @@
 #ifndef PARAMETERIZABLE_H
 #define PARAMETERIZABLE_H
 
+#ifdef REALTIME_CHECKS
+
+// If REALTIME_CHECKS is defined, we want to check for dynamic memory allocation.
+// Make Eigen check for dynamic memory allocation
+#define EIGEN_RUNTIME_NO_MALLOC
+// We define ENTERING_REAL_TIME_CRITICAL_CODE and EXITING_REAL_TIME_CRITICAL_CODE to start/stop
+// checking dynamic memory allocation
+#define ENTERING_REAL_TIME_CRITICAL_CODE Eigen::internal::set_is_malloc_allowed(false);
+#define EXITING_REAL_TIME_CRITICAL_CODE Eigen::internal::set_is_malloc_allowed(true);
+
+#else // REALTIME_CHECKS
+
+// REALTIME_CHECKS is not defined, not need to do any checks on real-time code. Simply set
+// ENTERING_REAL_TIME_CRITICAL_CODE and EXITING_REAL_TIME_CRITICAL_CODE to empty strings.
+#define ENTERING_REAL_TIME_CRITICAL_CODE
+#define EXITING_REAL_TIME_CRITICAL_CODE
+
+#endif // REALTIME_CHECKS
+
 #include <set>
 #include <string>
 #include <vector>
 #include <eigen3/Eigen/Core>
 
-#ifdef EIGEN_RUNTIME_NO_MALLOC
-// If EIGEN_RUNTIME_NO_MALLOC is defined, we want to check for dynamic memory allocation.
-// Thus we define ENTERING_REAL_TIME_CRITICAL_CODE and EXITING_REAL_TIME_CRITICAL_CODE to start/stop
-// checking dynamic memory allocation
-#define ENTERING_REAL_TIME_CRITICAL_CODE Eigen::internal::set_is_malloc_allowed(false);
-#define EXITING_REAL_TIME_CRITICAL_CODE Eigen::internal::set_is_malloc_allowed(true);
-#else 
-// EIGEN_RUNTIME_NO_MALLOC is not defined, not need to do any checks on real-time code. Simply set
-// ENTERING_REAL_TIME_CRITICAL_CODE and EXITING_REAL_TIME_CRITICAL_CODE to empty strings.
-#define ENTERING_REAL_TIME_CRITICAL_CODE
-#define EXITING_REAL_TIME_CRITICAL_CODE
-#endif
 
-// Put the whole EIGEN_RUNTIME_NO_MALLOC into a macro
-//    ENTERING_REAL_TIME_CRITICAL_CODE
-//    EXITING_REAL_TIME_CRITICAL_CODE
-//    Put in Parameterizable. Not ideal.
 
 #include <boost/serialization/nvp.hpp>
 
