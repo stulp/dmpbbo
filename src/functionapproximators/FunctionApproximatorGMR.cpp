e923181a3b6d7c1a29dd73f1bfb872810991c3ab
Freek Stulp <freek.stulp@ensta-paristech.fr>
2015-01-21 10:04:17 +0100

    Fixed docu

--- src/functionapproximators/FunctionApproximatorGMR.cpp
+++ src/functionapproximators/FunctionApproximatorGMR.cpp
@@ -200,7 +200,7 @@ double FunctionApproximatorGMR::normalPDF(const VectorXd& mu, const MatrixXd& co
   double output = exp(-0.5*(input-mu).transpose()*covar_inverse*(input-mu));
   // For invertible matrices (which covar apparently was), det(A^-1) = 1/det(A)
   // Hence the 1.0/covar_inverse.determinant() below
-  //  ( (2\pi)^N*|\Sigma| )^(-1/2)
+  //  ( (2*pi)^N*|\Sigma| )^(-1/2)
   output *= pow(pow(2*M_PI,mu.size())/covar_inverse.determinant(),-0.5);   
   return output;
 }
