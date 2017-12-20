36015fbc7920088b8f3e8658475a86855bce709f
Freek Stulp <Freek.Stulp@dlr.de>
2017-12-20 14:38:12 +0100

    Python no longer included in Doxygen documentation

--- python/dmp_bbo/task_solver.py
+++ python/dmp_bbo/task_solver.py
@@ -1,18 +1,15 @@
-## \file task_solver.py
-#  \author Freek Stulp
-#  \brief  TaskSolver class
-#  
-#  \ingroup Demos
-#  \ingroup DMP_BBO
-
-## Interface for classes that can perform rollouts.
-# For further information see the section on \ref sec_bbo_task_and_task_solver
 class TaskSolver:
+    """Interface for classes that can perform rollouts.
     
-    ## Perform rollouts, that is, given a set of samples, determine all the variables that are relevant to evaluating the cost function. 
-    # \param[in] sample The sample
-    # \return The variables relevant to computing the cost.
+    For further information see the section on \ref sec_bbo_task_and_task_solver
+    """
+    
     def performRollout(self,sample):
+        """ Perform rollouts, that is, given a set of samples, determine all the variables that are relevant to evaluating the cost function.         
+
+        \param[in] sample The sample
+        \return The variables relevant to computing the cost.
+        """
         raise NotImplementedError('subclasses must override performRollout()!')
         
     def plotRollout(self,cost_vars,ax):
