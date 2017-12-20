c1150d73290988937c4f6703b45e156d8916b893
Freek Stulp <Freek.Stulp@dlr.de>
2017-12-20 14:05:04 +0100

    Moved all todo items to special page

--- src/mainpage.hpp
+++ src/mainpage.hpp
@@ -65,8 +65,77 @@ The PEP Python naming conventions have been followed as much as possible, except
 See \ref page_serialization
 */
 
+/** \page page_todo Todo
+
+\section To decide
+
+Separate between understanding DMPs, and understanding the code? Or interleave?
+
+\section Things to document
+\li Python/C++ what to use for what?
+\li functionapproximator: Parameterizable
+\li functionapproximator: Unified Model
+\li functionapproximator: Model/Meta/Algorithm
+\li dynamicalsystems: differentialEquation vs analyticalSolution
+\li dmp: is a dynamical system (no analyticalSolution)
+\li dmp: is Parameterizable.
+\li dmp: DmpContextual
+\li bbo: Has nothing to do with DMPs. "Minimal CMA-ES"
+\li dmp_bbo: task and task solver
+
+\section For next release
+
+\li Fix warnings in "make Docs"
+
+\li get rid of runOptimizationParallelDeprecated(), and implement in UpdaterCovarAdapation
+
+\li Update documentation for parallel (No need for parallel in python, because only decay has been implemented for now)
+
+\li document Python classes/functions
+
+\li test DmpContextual
+
+\li "virtual int getExpectedOutputDim(void) const" should be pure virtual
+
+
+\section Perhaps for next release?
+
+\li Optimizations: Remove all occurences of inverse()
+http://eigen.tuxfamily.org/index.php?title=Talk:FAQ#Is_there_a_way_to_check_if_I.27m_accidentally_triggering_dynamic_memory_allocation_in_a_time_critical_block_of_code.3F  
+
+\li Should FunctionApproximators be friends with their ModelParameters?
+
+\li Gaussian Mixture Regression: 1) plot covariance matrices in Python 2) parameterizable: mu and priors, but not covars
+
+
+\li implement covar adaptation in python als (with blocks too)
+  
+\li implement generic regularization in Task
+
+\li in demoDmpChangeGoal: loop over 4 scaling methods. scaling method enumerator: NONE, AMPLITUDE, GOAL
+
+\li More coherent structure  
+\code{.cpp}
+  void predict(const MatrixXd& inputs, MatrixXd& predictions)
+  {
+    MatrixXd variances(0,0);
+    predict(inputs,predictions,variances)
+  }
+  virtual void predict(const MatrixXd& inputs, MatrixXd& predictions, Matrix& variances) = 0;
+\endcode  
+
+\section Long-term
+
+\li setColor on ellipses?
+
+
+
+
+ */ 
+
 /** \defgroup Demos Demos
  */
+ 
 
 /** Namespace used for all classes in the project.
  */
