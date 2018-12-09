Scaling to new goals works in Python now. Fixes #22

demos_python/dmp/demoDmpChangeGoal.py
@@ -47,7 +47,7 @@ def getDemoTrajectory(ts):
     if (use_viapoint_traj):
         n_dims = 2
         y_first = np.zeros(n_dims)
-        y_last  = 0.1*np.zeros(n_dims)
+        y_last  = 0.1*np.ones(n_dims)
         if (n_dims==2):
             y_last[1] = -0.8
             viapoint_time = 0.25
@@ -77,6 +77,10 @@ if __name__=='__main__':
     y_init = trajectory.initial_y()
     y_attr = trajectory.final_y()
     
+    #fig = plt.figure(1)
+    #axs1 = [ fig.add_subplot(231), fig.add_subplot(132), fig.add_subplot(133) ] 
+    #lines = plotTrajectory(trajectory.asMatrix(),axs1)
+    #plt.show()    
     
     n_dims = trajectory.dim()
 
@@ -91,11 +95,11 @@ if __name__=='__main__':
         
         # MAKE THE FUNCTION APPROXIMATORS
         function_apps = [ FunctionApproximatorLWR(10), FunctionApproximatorLWR(10)]
-        dmp = Dmp(tau, y_init, y_attr, function_apps,scaling)
+        sigmoid_max_rate=-20
+        dmp = Dmp(tau, y_init, y_attr, function_apps, "Dmp", sigmoid_max_rate, scaling)
 
         # CONSTRUCT AND TRAIN THE DMP
         directory_scaling = directory +"/"+ scaling
-        #    dmp->train(trajectory,directory_scaling,overwrite)
         dmp.train(trajectory)
         
         tau_exec = 0.7
@@ -111,11 +115,10 @@ if __name__=='__main__':
             # 4 => -0.5
             # 5 => -1.0
             # 6 => -1.5                         
-            y_attr  = trajectory.final_y()
-            y_attr *= (0.5*(goal_number-3))
+            y_attr_scaled = y_attr * (0.5*(goal_number-3))
            
             # ANALYTICAL SOLUTION 
-            dmp.set_attractor_state(y_attr)
+            dmp.set_attractor_state(y_attr_scaled)
             ( xs, xds, forcing_terms, fa_outputs) = dmp.analyticalSolution(ts)
             traj_reproduced = dmp.statesAsTrajectory(ts,xs,xds)
             basename = "reproduced" + str(goal_number)
@@ -125,6 +128,7 @@ if __name__=='__main__':
             xs_step = np.zeros([n_time_steps,dmp.dim_])
             xds_step = np.zeros([n_time_steps,dmp.dim_])
         
+            dmp.set_attractor_state(y_attr_scaled)
             (x,xd) = dmp.integrateStart()
             xs_step[0,:] = x
             xds_step[0,:] = xd

python/dmp/Dmp.py
@@ -144,17 +144,10 @@ class Dmp(DynamicalSystem,Parameterizable):
         forcing_term = gating*fa_output
         
   
-        #// Scale the forcing term, if necessary
-        self.forcing_term_scaling_ = "G_MINUS_Y0_SCALING"
-        #print(self.forcing_term_scaling_)
+        # Scale the forcing term, if necessary
         if (self.forcing_term_scaling_=="G_MINUS_Y0_SCALING"):
-            #print(self.initial_state_)
-            #print(self.attractor_state_)
             g_minus_y0 = (self.attractor_state_-self.initial_state_)
-            #print(g_minus_y0)
-            #print(forcing_term)
             forcing_term = forcing_term*g_minus_y0
-            #print(forcing_term)
         
         elif (self.forcing_term_scaling_=="AMPLITUDE_SCALING"):
             forcing_term = forcing_term*self.trajectory_amplitudes_
@@ -202,16 +195,14 @@ class Dmp(DynamicalSystem,Parameterizable):
         forcing_terms = fa_outputs*xs_gating
   
         # Scale the forcing term, if necessary
-        #if (forcing_term_scaling_==G_MINUS_Y0_SCALING)
-        #{
-        #MatrixXd g_minus_y0_rep = (attractor_state()-initial_state()).transpose().replicate(n_time_steps,1)
-        #forcing_terms = forcing_terms.array()*g_minus_y0_rep.array()
-        #}
-        #else if (forcing_term_scaling_==AMPLITUDE_SCALING)
-        #{
-        #MatrixXd trajectory_amplitudes_rep = trajectory_amplitudes_.transpose().replicate(n_time_steps,1)
-        #forcing_terms = forcing_terms.array()*trajectory_amplitudes_rep.array()
-        #}
+        if (self.forcing_term_scaling_=="G_MINUS_Y0_SCALING"):
+            g_minus_y0 = (self.attractor_state_-self.initial_state_)
+            g_minus_y0_rep = np.tile(g_minus_y0,(n_time_steps,1))
+            forcing_terms *= g_minus_y0_rep
+            
+        elif (self.forcing_term_scaling_=="AMPLITUDE_SCALING"):
+            trajectory_amplitudes_rep = np.tile(self.trajectory_amplitudes_,(n_time_steps,1))
+            forcing_terms *= trajectory_amplitudes_rep
   
   
         # Get current delayed goal
@@ -339,17 +330,16 @@ class Dmp(DynamicalSystem,Parameterizable):
         for dd in range(self.dim_orig_):
             f_target[:,dd] = f_target[:,dd]/np.squeeze(xs_gating)
   
-        #  // Factor out scaling
-        #  if (forcing_term_scaling_==G_MINUS_Y0_SCALING)
-        #  {
-        #    MatrixXd g_minus_y0_rep = (attractor_state()-initial_state()).transpose().replicate(n_time_steps,1)
-        #    f_target = f_target.array()/g_minus_y0_rep.array()
-        #  }
-        #  else if (forcing_term_scaling_==AMPLITUDE_SCALING)
-        #  {
-        #    MatrixXd trajectory_amplitudes_rep = trajectory_amplitudes_.transpose().replicate(n_time_steps,1)
-        #    f_target = f_target.array()/trajectory_amplitudes_rep.array()
-        #  }
+
+        # Factor out scaling
+        if (self.forcing_term_scaling_=="G_MINUS_Y0_SCALING"):
+            g_minus_y0 = (self.attractor_state_-self.initial_state_)
+            g_minus_y0_rep = np.tile(g_minus_y0,(n_time_steps,1))
+            f_target /= g_minus_y0_rep
+            
+        elif (self.forcing_term_scaling_=="AMPLITUDE_SCALING"):
+            trajectory_amplitudes_rep = np.tile(self.trajectory_amplitudes_,(n_time_steps,1))
+            f_target /= trajectory_amplitudes_rep
  
         return  (fa_inputs_phase, f_target)
 