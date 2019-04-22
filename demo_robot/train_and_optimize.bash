bc554a493b3eb3ebfe38d75b976dadfde1eff3af
Freek Stulp <Freek.Stulp@dlr.de>
2019-04-22 21:00:40 +0200

    Run the entire demo in a bash script

--- demo_robot/train_and_optimize.bash
+++ demo_robot/train_and_optimize.bash
@@ -4,7 +4,7 @@
 # STEP 1: Train the DMP with a trajectory
 
 # Train a dmp with a trajectory, and save it to a file
-./step1B_trainDmpFromTrajectoryFile trajectory.txt results/dmp.xml results/policy_parameters.txt results/train/ 6
+./step1A_trainDmpFromTrajectoryFile trajectory.txt results/dmp.xml results/policy_parameters.txt results/train/ 10
 
 # Plot the results of training the dmp
 python3 step1B_trainDmpFromTrajectoryFilePlot.py results/train/
@@ -13,7 +13,7 @@ python3 step1B_trainDmpFromTrajectoryFilePlot.py results/train/
 ################################
 # STEP 2: Define and save the task
 
-python3 step2_defineTask.py results/
+python3 step2_defineTask.py results/task.p
 
 ################################
 # STEP 3: Tune the exploration noise
@@ -21,14 +21,35 @@ python3 step2_defineTask.py results/
 # Generate some samples to tune the exploration, run the dmp, and plot the results
 # The penultimate parameters is the magnitude of the exploration. Here we try three values
 
-python3 step3A_tuneExploration.py results/policy_parameters.txt results/distribution_initial_covar.txt results/tune_exploration_0.1/ 0.1 12
-./step3B_performExplorationRollouts.bash results/dmp.xml results/tune_exploration_0.1/
-python3 step3C_tuneExplorationPlot.py results/tune_exploration_0.1/ results/task.p
+python3 step3A_tuneExploration.py results/policy_parameters.txt results/distribution_initial_covar.txt results/tune_exploration_1.0/ 1.0 12
+./step3B_performExplorationRollouts.bash results/dmp.xml results/tune_exploration_1.0/
+python3 step3C_tuneExplorationPlot.py results/tune_exploration_1.0/ results/task.p
 
 python3 step3A_tuneExploration.py results/policy_parameters.txt results/distribution_initial_covar.txt results/tune_exploration_10.0/ 10.0 12
 ./step3B_performExplorationRollouts.bash results/dmp.xml results/tune_exploration_10.0/
 python3 step3C_tuneExplorationPlot.py results/tune_exploration_10.0/ results/task.p
 
-python3 step3A_tuneExploration.py results/policy_parameters.txt results/distribution_initial_covar.txt results/tune_exploration_1.0/ 1.0 12
-./step3B_performExplorationRollouts.bash results/dmp.xml results/tune_exploration_1.0/
-python3 step3C_tuneExplorationPlot.py results/tune_exploration_1.0/ results/task.p
+python3 step3A_tuneExploration.py results/policy_parameters.txt results/distribution_initial_covar.txt results/tune_exploration_3.0/ 3.0 12
+./step3B_performExplorationRollouts.bash results/dmp.xml results/tune_exploration_3.0/
+python3 step3C_tuneExplorationPlot.py results/tune_exploration_3.0/ results/task.p
+
+################################
+# STEP 4: Run the optimization
+
+for i in {0..5}
+do
+  python3 step4A_oneOptimizationUpdate.py results/
+  ./step4B_performRollouts.bash results/  
+done
+
+# Plot intermediate results (after 3 updates)
+python3 step4C_plotOptimization.py results/
+
+for i in {0..4}
+do
+  python3 step4A_oneOptimizationUpdate.py results/
+  ./step4B_performRollouts.bash results/  
+done
+
+# Plot results (after 8 updates)
+python3 step4C_plotOptimization.py results/
