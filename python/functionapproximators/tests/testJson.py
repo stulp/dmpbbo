ebfcb3bb613f9a96faf546d6fa2d2773758747eb
Freek Stulp <Freek.Stulp@dlr.de>
2022-04-04 07:54:48 +0200

    Avoid compression of ndarray in jsonpickle

--- python/functionapproximators/tests/testJson.py
+++ python/functionapproximators/tests/testJson.py
@@ -21,8 +21,14 @@ import os, sys
 
 import json
 import jsonpickle
+from jsonpickle import handlers
+
 import jsonpickle.ext.numpy as jsonpickle_numpy
 jsonpickle_numpy.register_handlers()
+handler = jsonpickle.ext.numpy.NumpyNDArrayHandlerView(size_threshold=None)
+handlers.registry.unregister(np.ndarray)
+handlers.registry.register(np.ndarray, handler, base=True)
+    
 import pprint
 
 # Include scripts for plotting
@@ -51,19 +57,15 @@ def toJson(obj):
         new_dict[key] = val 
     return json.dumps(new_dict,cls=NumpyArrayEncoder)
 
-if __name__=='__main__':
-    """Run some training sessions and plot results."""
+def getTrainingData(n_dims):
 
-    np.set_printoptions(precision=4)
-
-    # Generate training data 
-    n_samples_per_dim = 25
-    #n_samples_per_dim = [11,9] # Does not work yet; kept for future debugging.
-    n_dims = 1 if np.isscalar(n_samples_per_dim) else len(n_samples_per_dim)
     if n_dims==1:
+        n_samples_per_dim = 25
         inputs = np.linspace(0.0, 2.0,n_samples_per_dim)
         targets = 3*np.exp(-inputs)*np.sin(2*np.square(inputs))
+    
     else:
+        n_samples_per_dim = [11,9] # Does not work yet; kept for future debugging.
         n_samples = np.prod(n_samples_per_dim)
         # Here comes naive inefficient implementation...
         x1s = np.linspace(-2.0, 2.0,n_samples_per_dim[0])
@@ -77,27 +79,39 @@ if __name__=='__main__':
                 inputs[ii,1] = x2
                 targets[ii] = 2.5*x1*np.exp(-np.square(x1)-np.square(x2))
                 ii += 1
-               
-    fa_names = ["RBFN","LWR"]
-    for fa_index in range(len(fa_names)):
-        fa_name = fa_names[fa_index]
-        
-        # Initialize function approximator
-        if fa_name=="LWR":
-            intersection = 0.5;
-            n_rfs = 9;
-            fa = FunctionApproximatorLWR(n_rfs,intersection)
-        else:
-            intersection = 0.7;
-            n_rfs = 9;
-            fa = FunctionApproximatorRBFN(n_rfs,intersection)
-        
-        # Train function approximator with data
-        fa.train(inputs,targets)
-        
-        #jsonpickle.set_encoder_options('json', indent=4)
-        #print(jsonpickle.encode(fa,cls=NumpyArrayEncoder))
-        j = json.dumps(json.loads(jsonpickle.encode(fa)), indent=4, sort_keys=True)
-        with open(fa_name+".json", "w") as text_file:
-            text_file.write(j)
+    return (inputs, targets)
+
+if __name__=='__main__':
+    """Run some training sessions and plot results."""
+
+    for n_dims in [1,2]:
+
+        (inputs, targets) = getTrainingData(n_dims)
+                   
+        for fa_name in ["RBFN","LWR"]:
+            
+            n_bfs = 9 if n_dims==1 else [5,5]
+            
+            # Initialize function approximator
+            if fa_name=="LWR":
+                intersection = 0.5;
+                fa = FunctionApproximatorLWR(n_bfs,intersection)
+            else:
+                intersection = 0.7;
+                fa = FunctionApproximatorRBFN(n_bfs,intersection)
+            
+            # Train function approximator with data
+            fa.train(inputs,targets)
+            
+            #jsonpickle.set_encoder_options('json', indent=4)# Didn't work
+            
+            # First do the pickle
+            jp = jsonpickle.encode(fa)
+            # Then load/dump for pretty formatting
+            #j = json.dumps(json.loads(jp), cls=NumpyArrayEncoder, indent=2, sort_keys=False)
+            j = json.dumps(json.loads(jp), indent=2, sort_keys=False)
+            
+            # Save to file
+            with open(f"{fa_name}_{n_dims}D.json", "w") as text_file:
+                text_file.write(j)
 
