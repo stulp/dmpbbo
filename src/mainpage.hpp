3614aebf7e4671e5fe40450ff7bc7af0aa699a58
Freek Stulp <Freek.Stulp@dlr.de>
2022-04-13 20:45:32 +0200

    Documentation

--- src/mainpage.hpp
+++ src/mainpage.hpp
@@ -4,59 +4,54 @@
  * @author Freek Stulp
  */
 
+ 
+/** Namespace used for all classes in the project.
+ */
+namespace DmpBBO {
+}
+
 /** \mainpage
 
 \section sec_cui_bono What the doxygen documentation is for
 
-This is the doxygen documentation of the DmpBbo library. Its main aim is to
-document the API, describe the implemenation, and provide rationale management
-for developers. If you are more interested in the theory behind dynamical
+This is the doxygen documentation of the C++ of the dmpbbo library. Its main aim is to
+document the C++ API, describe the implemenation, and provide rationale management (see \ref
+page_design) for developers:
+
+\li If you want to get started quickly with the C++ implementation only, the <a
+href="https://github.com/stulp/dmpbbo/blob/master/demos_cpp">demos</a> would be
+the right place. 
+
+\li If you are more interested in the theory behind dynamical
 movement primitives and their optimization, the <a
 href="https://github.com/stulp/dmpbbo/blob/master/tutorial">tutorials</a> is the
-place to go for you. If you want to get started quickly, the <a
-href="https://github.com/stulp/dmpbbo/blob/master/demos_cpp">demos</a> would be
-the right place.
+place to go for you.
+
+\li If you would like to train function approximators and DMPs, please do so 
+in the Python code, and export the result to json with jsonpickle. This C++ library can read
+those json files and execute the resulting DMPs. See also the page on <a
+href="https://github.com/stulp/dmpbbo/blob/master/tutorial/python_cpp.md">Python and C++</a>.
+
 
 \section sec_overview_modules Overview of the modules/libraries
 
-This library contains several modules for training dynamical movement primitives
-(DMPs), and optimizing their parameters through black-box optimization. Each
-module has its own dedicated page.
+This library contains several modules for executing dynamical movement primitives
+(DMPs). Each module has its own dedicated page.
 
+\li \ref eigenutils (in eigenutils/) This header-only module provides utilities
+for the Eigen matrix library (file IO, (de)serialization with JSON, and checking whether
+code is real-time). 
 
 \li  \ref page_dyn_sys (in dynamicalsystems/) This module provides
 implementations of several basic dynamical systems. DMPs are combinations of
-such systems. This module is completely independent of all other modules.
+such systems.
 
 \li \ref page_func_approx (in functionapproximators/) This module provides
-implementations (but mostly wrappers around external libraries) of several
-function approximators. DMPs use function approximators to learn and reproduce
-arbitrary smooth movements. This module is completely independent of all other
-modules.
+implementations of several function approximators. DMPs use function approximators to learn and reproduce arbitrary smooth movements.
 
 \li \ref page_dmp (in dmp/) This module provides an implementation of several
-types of DMPs. It depends on both the DynamicalSystems and FunctionApproximators
-modules, but no other.
+types of DMPs.
 
-\li \ref page_bbo (in bbo/) This module provides implementations of several
-stochastic optimization algorithms for the optimization of black-box cost
-functions. This module is completely independent of all other modules.
-
-\li \ref page_dmp_bbo (in dmp_bbo/) This module applies black-box optimization
-to the parameters of a DMP. It depends on all the other modules.
-
-\section sec_pytho_cpp A mixed C++/Python library
-
-
-A part of the functionality of the C++ code has been mirrored in Python. The
-Python version is probably the better language for getting to know dmpbbo
-(especially if you do not know C++ ;-) The C++ code is the better choice if you
-want to run dmpbbo on a real robot in a real-time environment. For now the
-Python code has not been documented well, please navigate the C++ documentation
-instead (class/function names have been kept consistent).
-
-Some general considerations on the design of the library are here \ref
-page_design
 
 */
 
@@ -74,16 +69,7 @@ use PIMPL; it is not so legible for the uninitiated user.
 \li I learned to use Eigen whilst coding this project (learning-by-doing). So
 especially the parts I coded first might have some convoluted solutions (I
 didn't learn about Eigen::Ref til later...). Any suggestions for making the code
-more legible or efficient are welcome. The same goes for Python actually. So be
-gentle on me on this one; I myself will probably look back at this Python code
-in a few years and think: "How cute... I was just a Python baby when I coded
-that."
-
-\li For consistency the names of Python modules (e.g. distribution_gaussian.py)
-have been kept consistent with their respective C++ implementation (e.g.
-DistributionGaussian.cpp). Several functions could also have been made mor
-Pythonic by exploiting duck-typing, but again, this has not always been done to
-keep consistency with the C++ code (where duck-typing is not possible).
+more legible or efficient are welcome. 
 
 \li For the organization of the code (directory structure), I went with this
 suggestion:
@@ -93,11 +79,12 @@ http://stackoverflow.com/questions/13521618/c-project-organisation-with-gtest-cm
 const) and then outputs (if they are not const, they are inputs for sure).
 Exception: if input arguments have default values, they can come after outputs.
 Virtual functions should not have default function arguments (this is confusing
-in the derived classes). If they really need them, then you have to make
-different functions with different argument lists (see for example
-DmpContextual::train(), there are 6 of them for this reason).
+in the derived classes).
 
-\section sec_naming Naming convention
+\section sec_naming Coding style
+
+Formatting according to the Google style (https://google.github.io/styleguide/cppguide.html) is done automatically with `clang-format'
+The only difference to the Google formatting style is that the opening bracket after a function header is on a newline, as this improves legibility for me. See the `.clang-format` settings file in the root of the repo.
 
 I mainly follow the following naming style:
 https://google.github.io/styleguide/cppguide.html#Naming
@@ -117,14 +104,9 @@ Exceptions to the style guide above:
 The PEP Python naming conventions have been followed as much as possible, except
 for functions, which are camelCased, for consistency with the C++ code.
 
-\section Serialization
-
-See \ref page_serialization
 */
 
 /** \page page_todo Todo
-
-\todo Explain why C++ implementation of bbo and dmp_bbo have only limited use.
 
 \todo Documentation: Write a related pages with a table on which functionality
 is implemented in Python/Cpp
@@ -194,44 +176,10 @@ generate such trajectories."
  the md files in the dmpbbo/demos_cpp/ directory.
  */
 
-/** \page page_moved Pages that have moved to the on-line tutorial (Markdown)
-
-\section page_unified_model Unified Model for Function Approximators
-
-This page has moved to <a
-href="https://github.com/stulp/dmpbbo/blob/master/tutorial/functionapproximators.md#unified-model-for-function-approximators"
-target="_blank">tutorial/functionapproximators.md</a>
-
-\section page_dmp_bbo Black Box Optimization of Dynamical Movement Primitives
-
-The documentation for the dmp_bbo module is in the tutorial <a
-href="https://github.com/stulp/dmpbbo/blob/master/tutorial/dmp_bbo.md"
-target="_blank">tutorial/dmp_bbo.md</a>.
-
-\section sec_bbo_task_and_task_solver CostFunction vs Task/TaskSolver
-
-This page has moved to <a
-href="https://github.com/stulp/dmpbbo/blob/master/tutorial/dmp_bbo.md#costfunction-vs-tasktasksolver"
-target="_blank">tutorial/dmp_bbo.md</a>
-
-
-\section sec_cost_components Cost components
-
-This page has moved to <a
-href="https://github.com/stulp/dmpbbo/blob/master/tutorial/dmp_bbo.md#cost-components"
-target="_blank">tutorial/dmp_bbo.md</a>
-
-\section sec_cost_vars Cost-relevant variables
-
-This page has moved to <a
-href="https://github.com/stulp/dmpbbo/blob/master/tutorial/dmp_bbo.md#cost-relevant-variables"
-target="_blank">tutorial/dmp_bbo.md</a>
-
-\section bibliography Bibliography
+/** \page page_bibliography Bibliography
 
 To ensure that all relevant entries are generated for the bibliography, here is
 a list.
-
 
 \cite buchli11learning
 \cite ijspeert02movement
@@ -253,7 +201,3 @@ a list.
 
  */
 
-/** Namespace used for all classes in the project.
- */
-namespace DmpBBO {
-}
