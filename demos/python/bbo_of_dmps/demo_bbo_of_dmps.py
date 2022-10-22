0781bc4cfed32ce456fa9f18c228f4cc2956bc96
Freek Stulp <Freek.Stulp@dlr.de>
2022-10-22 12:02:53 +0200

    Demos can take trajectory now

--- demos/python/bbo_of_dmps/demo_bbo_of_dmps.py
+++ demos/python/bbo_of_dmps/demo_bbo_of_dmps.py
@@ -15,9 +15,10 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with DmpBbo.  If not, see <http://www.gnu.org/licenses/>.
 """Script for bbo_of_dmps demo."""
-
-
+import argparse
 import sys
+import tempfile
+from pathlib import Path
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -27,37 +28,24 @@ from dmpbbo.bbo.updaters import UpdaterCovarAdaptation, UpdaterCovarDecay, Updat
 from dmpbbo.bbo_of_dmps.run_optimization_task import run_optimization_task
 from dmpbbo.bbo_of_dmps.TaskSolverDmp import TaskSolverDmp
 from dmpbbo.dmps.Dmp import Dmp
+from dmpbbo.dmps.Trajectory import Trajectory
 from dmpbbo.functionapproximators.FunctionApproximatorRBFN import FunctionApproximatorRBFN
 from TaskViapoint import TaskViapoint
 
 
-def run_demo(directory, n_dims):
-    """ Run one demo for bbo_of_dmps.
+def run_demo(directory, traj):
+    """ Run one demo for bbo_of_dmps (with single updates)
 
     @param directory: Directory to save results to
-    @param n_dims: Number of dimensions of the task (i.e. the viapoint)
+    @param traj: Initial trajectory
     """
 
-    # Some DMP parameters
-    tau = 0.5
-    y_init = np.linspace(1.8, 2.0, n_dims)
-    y_attr = np.linspace(4.0, 3.0, n_dims)
+    n_dims = traj.dim
+    n_basis = 10
+    h = 0.9  # intersection_height
+    function_apps = [FunctionApproximatorRBFN(n_basis, h) for _ in range(n_dims)]
 
-    # initialize function approximators with random values
-    function_apps = []
-    intersection_height = 0.8
-    for n_basis in [6, 7]:
-        fa = FunctionApproximatorRBFN(n_basis, intersection_height)
-        fa.train(np.linspace(0, 1, 100), np.zeros(100))
-
-        fa.set_selected_param_names("weights")
-        random_weights = 10.0 * np.random.normal(0, 1, n_basis)
-        fa.set_param_vector(random_weights)
-
-        function_apps.append(fa)
-
-    # Initialize Dmp
-    dmp = Dmp(tau, y_init, y_attr, function_apps)
+    dmp = Dmp.from_traj(traj, function_apps)
     dmp.set_selected_param_names("weights")
     # dmp.set_selected_param_names(['goal','weights'])
 
@@ -72,8 +60,8 @@ def run_demo(directory, n_dims):
         viapoint,
         viapoint_time=viapoint_time,
         viapoint_radius=0.1,
-        goal=y_attr,
-        goal_time=1.1 * tau,
+        goal=dmp.y_attr,
+        goal_time=1.1 * dmp.tau,
         viapoint_weight=1.0,
         acceleration_weight=0.00005,
         goal_weight=0.0,
@@ -117,11 +105,28 @@ def run_demo(directory, n_dims):
 
 def main():
     """ Main function of the script. """
-    directory = sys.argv[1] if len(sys.argv) > 1 else None
+    default_dir = Path(tempfile.gettempdir(), "dmpbbo", "demo_bbo_of_dmps")
 
-    for n_dims in [1, 2]:
-        print(f"Optimization of {n_dims}-D task")
-        run_demo(directory, n_dims)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", type=str, help="dir to write results to", default=default_dir)
+    parser.add_argument("--traj", type=str, help="optional init trajectory file")
+    args = parser.parse_args()
+
+    if args.traj:
+        traj = Trajectory.loadtxt(args.traj)
+        # traj.plot # Check if trajectory loaded correctly.
+        run_demo(args.dir, traj)
+
+    else:
+        # Some DMP parameters
+        for n_dims in [1, 2]:
+            tau = 0.5
+            dt = 0.005
+            ts = np.linspace(0.0, tau, int(tau/dt)+1)
+            y_init = np.linspace(1.8, 2.0, n_dims)
+            y_attr = np.linspace(4.0, 3.0, n_dims)
+            traj = Trajectory.from_min_jerk(ts, y_init, y_attr)
+            run_demo(args.dir, traj)
 
     plt.show()
 
